9e2aaa9d6dad2b0c829b7957715721481f068f51
ryuv <sujea777@naver.com>
2020-11-07 09:21:26 +0000

    1차 완료

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Formik } from "formik"
+import { Formik,Field } from "formik"
 import axios from 'axios'
 
 export default class App extends Component {
@@ -22,6 +22,14 @@ export default class App extends Component {
   //     });
   
   // }
+
+  htmlTag(str){
+    if(str !== ""){
+      return str.replaceAll("<", "%26lt;")
+      .replaceAll(">", "%26gt;")
+      .replaceAll("/", "%26%2347;");
+    }
+  }
 
 
   render() {
@@ -51,19 +59,39 @@ export default class App extends Component {
           kakaoTitle: "",
           kakaoContent:"",
           mainPhoto:null,
-          galleryPhoto:null,
           husbandPhoto:null,
-          wifePhoto:null
+          wifePhoto:null,
+          galleryPhoto_1:null,
+          galleryPhoto_2:null,
+          galleryPhoto_3:null,
+          galleryPhoto_4:null,
+          galleryPhoto_5:null,
+          galleryPhoto_6:null,
+          galleryPhoto_7:null,
+          galleryPhoto_8:null,
+          galleryPhoto_9:null,
+          galleryPhoto_10:null,
+
         }} 
           onSubmit={(data, { setSubmitting }) => {
           setSubmitting(true);
-          console.log(data);
+          console.log( this.htmlTag(data.weddingHow));
 
           const formData = new FormData();
           formData.append('mainPhoto',data.mainPhoto);
-          formData.append('galleryPhoto',data.galleryPhoto);
           formData.append('husbandPhoto',data.husbandPhoto);
           formData.append('wifePhoto',data.wifePhoto);
+          formData.append('galleryPhoto_1',data.galleryPhoto_1);
+          formData.append('galleryPhoto_2',data.galleryPhoto_2);
+          formData.append('galleryPhoto_3',data.galleryPhoto_3);
+          formData.append('galleryPhoto_4',data.galleryPhoto_4);
+          formData.append('galleryPhoto_5',data.galleryPhoto_5);
+          formData.append('galleryPhoto_6',data.galleryPhoto_6);
+          formData.append('galleryPhoto_7',data.galleryPhoto_7);
+          formData.append('galleryPhoto_8',data.galleryPhoto_8);
+          formData.append('galleryPhoto_9',data.galleryPhoto_9);
+          formData.append('galleryPhoto_10',data.galleryPhoto_10);
+
 
           // this.register(formData)
           const config = {
@@ -71,11 +99,10 @@ export default class App extends Component {
                 'content-type': 'multipart/form-data'
               }
           };
-          // axios.post("http://www.cryucodes.design:9091/api/upload",formData,config)
-          //     .then((res) => {
-          //       console.log(res);
-          //     }).catch((error) => {
-          // });
+          axios.post("http://www.cryucodes.design:9091/api/upload",formData,config)
+          .then((res) => {
+            console.log(res);
+          }).catch((error) => {});
 
           axios.get(`http://www.cryucodes.design:9091/api/add?husbandName=${
             data.husbandName
@@ -112,21 +139,54 @@ export default class App extends Component {
           }&weddingNumber=${
             data.weddingNumber
           }&weddingHow=${
-            data.weddingHow
+            this.htmlTag(data.weddingHow)
           }&weddingAddress=${
             data.weddingAddress
           }&invatationContent=${
-            data.invatationContent
+            this.htmlTag(data.invatationContent)
           }&kakaoTitle=${
             data.kakaoTitle
           }&kakaoContent=${
             data.kakaoContent
           }&mainPhoto=${
-            data.mainPhoto.name
+            data.mainPhoto != null?
+              data.mainPhoto.name:""
           }&husbandPhoto=${
-            data.husbandPhoto.name
+            data.husbandPhoto != null?
+              data.husbandPhoto.name:""
           }&wifePhoto=${
-            data.wifePhoto.name
+            data.wifePhoto != null?
+              data.wifePhoto.name:""
+          }&galleryPhoto_1=${
+            data.galleryPhoto_1 != null?
+              data.galleryPhoto_1.name:""
+          }&galleryPhoto_2=${
+            data.galleryPhoto_2 != null?
+              data.galleryPhoto_2.name:""
+          }&galleryPhoto_3=${
+            data.galleryPhoto_3 != null?
+              data.galleryPhoto_3.name:""
+          }&galleryPhoto_4=${
+            data.galleryPhoto_4 != null?
+              data.galleryPhoto_4.name:""
+          }&galleryPhoto_5=${
+            data.galleryPhoto_5 != null?
+              data.galleryPhoto_5.name:""
+          }&galleryPhoto_6=${
+            data.galleryPhoto_6 != null?
+              data.galleryPhoto_6.name:""
+          }&galleryPhoto_7=${
+            data.galleryPhoto_7 != null?
+              data.galleryPhoto_7.name:""
+          }&galleryPhoto_8=${
+            data.galleryPhoto_8 != null?
+              data.galleryPhoto_8.name:""
+          }&galleryPhoto_9=${
+            data.galleryPhoto_9 != null?
+              data.galleryPhoto_9.name:""
+          }&galleryPhoto_10=${
+            data.galleryPhoto_10 != null?
+              data.galleryPhoto_10.name:""
           }
           `)
           .then(res=>{console.log(res)});
@@ -221,8 +281,13 @@ export default class App extends Component {
                 </div>
                 <div>
                   <h3>결혼식장 가는 방법</h3>
-                  <input name="weddingHow" value={values.weddingHow} onChange={handleChange}/>
+                  <Field
+                    name="weddingHow"
+                    component="textarea"
+                    rows="10"
+                  />
                 </div>
+                
                 <div>
                   <h3>결혼식장 주소</h3>
                   <input name="weddingAddress" value={values.weddingAddress} onChange={handleChange}/>
@@ -234,7 +299,11 @@ export default class App extends Component {
 
                 <div>
                   <h3>청첩장 내용</h3>
-                  <input name="invatationContent" value={values.invatationContent} onChange={handleChange}/>
+                  <Field
+                    name="invatationContent"
+                    component="textarea"
+                    rows="10"
+                  />
                 </div>
 
                 <div>
@@ -244,7 +313,11 @@ export default class App extends Component {
 
                 <div>
                   <h3>상세 내용 2줄</h3>
-                  <input name="kakaoContent" value={values.kakaoContent} onChange={handleChange}/>
+                  <Field
+                    name="kakaoContent"
+                    component="textarea"
+                    rows="10"
+                  />
                 </div>
 
                 {/* <div>
@@ -264,11 +337,101 @@ export default class App extends Component {
                 </div>
 
                 <div>
-                  <h3>갤러리 사진 1~10</h3>
-                  <input type="file" multiple
-                  name="galleryPhoto"   
+                  <h3>갤러리 사진 1</h3>
+                  <input type="file"
+                  name="galleryPhoto_1"   
                   onChange={(event) =>{
-                    setFieldValue("galleryPhoto", event.currentTarget.files);
+                    setFieldValue("galleryPhoto_1", event.currentTarget.files[0]);
+                  }}
+                  />
+                </div>
+
+                <div>
+                  <h3>갤러리 사진 2</h3>
+                  <input type="file"
+                  name="galleryPhoto_2"   
+                  onChange={(event) =>{
+                    setFieldValue("galleryPhoto_2", event.currentTarget.files[0]);
+                  }}
+                  />
+                </div>
+
+                <div>
+                  <h3>갤러리 사진 3</h3>
+                  <input type="file"
+                  name="galleryPhoto_3"   
+                  onChange={(event) =>{
+                    setFieldValue("galleryPhoto_3", event.currentTarget.files[0]);
+                  }}
+                  />
+                </div>
+
+                <div>
+                  <h3>갤러리 사진 4</h3>
+                  <input type="file"
+                  name="galleryPhoto_4"   
+                  onChange={(event) =>{
+                    setFieldValue("galleryPhoto_4", event.currentTarget.files[0]);
+                  }}
+                  />
+                </div>
+
+                <div>
+                  <h3>갤러리 사진 5</h3>
+                  <input type="file"
+                  name="galleryPhoto_5"   
+                  onChange={(event) =>{
+                    setFieldValue("galleryPhoto_5", event.currentTarget.files[0]);
+                  }}
+                  />
+                </div>
+
+                <div>
+                  <h3>갤러리 사진 6</h3>
+                  <input type="file"
+                  name="galleryPhoto_6"   
+                  onChange={(event) =>{
+                    setFieldValue("galleryPhoto_6", event.currentTarget.files[0]);
+                  }}
+                  />
+                </div>
+
+                <div>
+                  <h3>갤러리 사진 7</h3>
+                  <input type="file"
+                  name="galleryPhoto_7"   
+                  onChange={(event) =>{
+                    setFieldValue("galleryPhoto_7", event.currentTarget.files[0]);
+                  }}
+                  />
+                </div>
+
+                <div>
+                  <h3>갤러리 사진 8</h3>
+                  <input type="file"
+                  name="galleryPhoto_8"   
+                  onChange={(event) =>{
+                    setFieldValue("galleryPhoto_8", event.currentTarget.files[0]);
+                  }}
+                  />
+                </div>
+
+                <div>
+                  <h3>갤러리 사진 9</h3>
+                  <input type="file"
+                  name="galleryPhoto_9"   
+                  onChange={(event) =>{
+                    setFieldValue("galleryPhoto_9", event.currentTarget.files[0]);
+                  }}
+                  />
+                </div>
+
+                <div>
+                  <h3>갤러리 사진 10</h3>
+                  <input type="file"
+                  name="galleryPhoto_10"   
+                  onChange={(event) =>{
+                    setFieldValue("galleryPhoto_10", event.currentTarget.files[0]);
                   }}
                   />
                 </div>
